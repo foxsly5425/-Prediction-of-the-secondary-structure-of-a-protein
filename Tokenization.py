71d770e376c6586568c838476e3d38ac8d9f36c9
foxsly5425 <146886753+foxsly5425@users.noreply.github.com>
2024-12-25 21:06:39 +0300

    Update Tokenization.py

--- Tokenization.py
+++ Tokenization.py
@@ -1,26 +1,3 @@
-import warnings
-warnings.filterwarnings("ignore")
-
-import json
-import math
-import time
-import numpy               as np
-import torch
-import torch.nn            as nn
-import torch.nn.functional as F
-import matplotlib.pyplot   as plt
-
-import wandb
-
-from sklearn.model_selection import train_test_split
-from sklearn.metrics         import f1_score, precision_score, recall_score
-from torch.utils.data        import Dataset, DataLoader
-from collections             import Counter
-from tqdm                    import tqdm
-
-from transformers            import BertModel, BertTokenizer
-
-
 START_TOKEN = 'X'
 END_TOKEN   = 'X'
 n_gramm     = 4
